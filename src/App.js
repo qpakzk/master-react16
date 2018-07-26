0f7ca88a6ec12a70475ce6f9c3faf9121a65f6ea
Sangwon Hong <qpakzk@gmail.com>
2018-07-27 01:33:46 +0900

    src: exercise about return
    * support Fragment Component
    * support return string

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
+class ReturnTypes extends Component {
+  render() {
+    return "hello";
+  }
+}
 class App extends Component {
   render() {
     return (
-      <div className="App"/>
+      <Fragment>
+        <ReturnTypes />
+      </Fragment>
     );
   }
 }
