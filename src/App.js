aa27dcc53895d9762858dafab615a70e68ac88a1
Sangwon Hong <qpakzk@gmail.com>
2018-07-27 02:17:34 +0900

    src: error handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import { createPortal } from 'react-dom';
+
+class ErrorMaker extends Component {
+  state = {
+    friends: ["Frodo", "Eric", "Billy", "Sangwon"]
+  }
+  
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  }
+
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById("touchme"));
@@ -13,12 +33,28 @@ class ReturnTypes extends Component {
     return "hello";
   }
 }
+
+const ErrorFallback = () => " Sorry Something went wrong";
 class App extends Component {
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch = (err, info) => {
+    console.log(`${err}, ${JSON.stringify(info)}`);
+    this.setState({
+      hasError: true
+    });
+  }
+
   render() {
+    const { hasError } = this.state;
+
     return (
       <Fragment>
         <Portals />
         <ReturnTypes />
+        {hasError ? <ErrorFallback/> : <ErrorMaker /> }
       </Fragment>
     );
   }
