5d18ef2abe1ab13d83b9752d6c6c5579ab183bfe
Sangwon Hong <qpakzk@gmail.com>
2018-07-27 02:30:44 +0900

    higher order components (HOC)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React, { Component, Fragment } from 'react';
 import { createPortal } from 'react-dom';
 
+const BoundaryHOC = ProtectedComponent => class Boundary extends Component {
+  state = {
+    hasError: false
+  }
+
+  componentDidCatch = () => {
+    this.setState({
+      hasError: true
+    });
+  };
+
+  render() {
+    const { hasError } = this.state;
+
+    if(hasError)
+      return <ErrorFallback />
+    else 
+      return <ProtectedComponent />
+  }
+};
+
 class ErrorMaker extends Component {
   state = {
     friends: ["Frodo", "Eric", "Billy", "Sangwon"]
@@ -20,11 +41,15 @@ class ErrorMaker extends Component {
   }
 }
 
+const PErrorMaker = BoundaryHOC(ErrorMaker);
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById("touchme"));
   }
 }
+
+const PPortals = BoundaryHOC(Portals);
 
 const Message = () => "Just touch it!";
 
@@ -36,28 +61,15 @@ class ReturnTypes extends Component {
 
 const ErrorFallback = () => " Sorry Something went wrong";
 class App extends Component {
-  state = {
-    hasError: false
-  };
-
-  componentDidCatch = (err, info) => {
-    console.log(`${err}, ${JSON.stringify(info)}`);
-    this.setState({
-      hasError: true
-    });
-  }
-
   render() {
-    const { hasError } = this.state;
-
     return (
       <Fragment>
-        <Portals />
+        <PPortals />
         <ReturnTypes />
-        {hasError ? <ErrorFallback/> : <ErrorMaker /> }
+        <PErrorMaker />
       </Fragment>
     );
   }
 }
 
-export default App;
+export default BoundaryHOC(App);
